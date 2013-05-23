84e18f5a63f0c33f346c11182214a4dd92180e6d
panoshuang <295338444@qq.com>
2013-05-23 21:48:49 +0800

    Initial Commit

---
+++ Cosmetology/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Cosmetology
+//
+//  Created by mijie on 13-5-23.
+//  Copyright (c) 2013年 pengpai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Cosmetology/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Cosmetology
+//
+//  Created by mijie on 13-5-23.
+//  Copyright (c) 2013年 pengpai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
